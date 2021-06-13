1ac63b7bf92a254f4e0de38ba4ad795aad796159
Gosia <malgorzata.g.tyrka@gmail.com>
2021-06-13 23:25:33 +0200

    Added monthly, weekly & todo asap functions

--- views/order_total.sql
+++ views/order_total.sql
@@ -7,3 +7,21 @@ RETURN
     FROM "order"
     WHERE "order".id = @OrderID
 )
+
+CREATE or alter FUNCTION disc_orders_lastmonth(@OrderID INTEGER)
+    RETURNS TABLE
+        AS
+        RETURN
+            (
+                SELECT "order".id, price_netto, price_brutto, created_time, ready_time
+                FROM "order"
+                WHERE "order".id = @OrderID AND "order".discount_type IS NOT NULL AND MONTH("order".created_time) = MONTH(GETDATE()) - 1
+            )
+
+-- to check after generating data
+CREATE
+    OR ALTER
+    VIEW disc_given
+AS SELECT "order".discount_type, count(discount_type)
+   FROM "order"
+   GROUP BY "order".discount_type
